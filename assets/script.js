2bb75b6715b10960b60b392d308d2f6268d439ca
Celina Louissaint <celinalouissaint@gmail.com>
2021-02-20 14:30:37 -0800

    added js file to make images links to projects

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+let cceImage = document.querySelector(".cce-image");
+let truliaImage = document.querySelector(".trulia-image");
+let airbnbImage = document.querySelector(".airbnb-image");
+
+cceImage.addEventListener("click", () => {
+    window.open("https://celinalou92.github.io/CCE-Website/");
+});
+
+truliaImage.addEventListener("click", () => {
+    window.open("https://codepen.io/celinalou92/pen/MWwewWQ");
+});
+
+airbnbImage.addEventListener("click", () => {
+    window.open("https://codepen.io/celinalou92/pen/abObXBy");
+});
